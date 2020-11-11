91a017580dd11b13c2798ec5c8d0d396f7ca377d
Greg Skluzacek <greg.skluzacek@nielsen.com>
2020-11-11 16:42:17 -0600

    changed multi thread test program to POC
    The POC demonstrates how the main thread adds the top level dirctories
    to be scanned as the initial items on the dir_queueu, the dir_workers then
    process the directories in the the dir_queue
    - add themselves to the output queue
    - gets a directory listing for themselves
    - for each item in the listing
    - either add it to the output queue if it is a file
    - or addits it to the dir_queue to be processed
    
    another output worker reads the items from the output queue
    and processes them (prints to output)
    
    shows how we can track which top level directory generated an item
    as well as what dir_worker processed the item

--- mt_test.py
+++ mt_test.py
@@ -1,35 +1,70 @@
-from threading import Thread, current_thread
+import os
+from threading import Thread
 from Queue import Queue
-from random import randint
-from time import sleep
 
 
-def worker(q):
+class DirItm:
+    def __init__(self, scan_index, thrd_nbr, di_typ, path):
+        self.scan_index = scan_index
+        self.thrd_nbr = thrd_nbr
+        self.di_typ = di_typ
+        self.path = path
+
+
+def dir_worker(thrd_nbr, dq, pq):
     while True:
-        item = q.get()
-        t = randint(3, 8)
-        print "Thread: {} Value: {} - will sleep for {} seconds".format(current_thread().name, item, t)
-        sleep(t)
-        q.task_done()
+        dir_item = dq.get()
+
+        pq.put(dir_item)
+
+        list_dir = os.listdir(dir_item.path)
+        for item in list_dir:
+            path = os.path.join(dir_item.path, item)
+            if os.path.isfile(path):
+                pq.put(DirItm(dir_item.scan_index, thrd_nbr, 'F', path))
+            else:
+                dq.put(DirItm(dir_item.scan_index, thrd_nbr, 'D', path))
+
+        dq.task_done()
 
 
-def main(source, num_worker_threads):
-    q = Queue()
+def path_worker(pq):
+    while True:
+        item = pq.get()
+        print '{}\t{}\t{}\t{}'.format(item.scan_index, item.thrd_nbr, item.di_typ, item.path)
+        pq.task_done()
+
+
+def main(top_lvl_dirs, num_worker_threads):
+    dir_queue = Queue()
+    path_queue = Queue()
+
+    # start the dir_worker threads (1 or more)
     for i in range(num_worker_threads):
-        t = Thread(target=worker, args=(q, ))
+        t = Thread(target=dir_worker, args=(i + 1, dir_queue, path_queue))
         t.daemon = True
         t.start()
 
-    print "putting things into the queue..."
-    for item in source:
-        q.put(item)
+    # start the path_worker thread (only 1)
+    t = Thread(target=path_worker, args=(path_queue, ))
+    t.daemon = True
+    t.start()
 
-    print "waiting for all items to be processed..."
-    q.join()
-    print "done !!!"
+    # add the top level directory to the dir_queue
+    for i, top_lvl_dir in enumerate(top_lvl_dirs):
+        dir_queue.put(DirItm(i, 0, 'D', top_lvl_dir))
+
+    # print "waiting for directory to be scanned..."
+    dir_queue.join()
+    # print "waiting for paths to be saved..."
+    path_queue.join()
+    # print "done !!!"
 
 
 if __name__ == '__main__':
-    src = range(20)
-    wrkr_threads = 8
-    main(src, wrkr_threads)
+    dirs_to_scan = [
+        '/Users/gskluzacek/Documents/ansbl_prj',
+        '/Users/gskluzacek/Documents/cr6-contents'
+    ]
+    wrkr_threads = 10
+    main(dirs_to_scan, wrkr_threads)
